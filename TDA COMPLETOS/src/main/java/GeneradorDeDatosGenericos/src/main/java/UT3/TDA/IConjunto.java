af48c254b8ad228f9ca1f964efa72db1b38d2e9e
Agustin Muniz <141841094+AgustinMunizF@users.noreply.github.com>
2024-07-04 02:18:36 -0300

    Delete TDA COMPLETOS/src/main/java/GeneradorDeDatosGenericos/src/main/java/UT3 directory

--- TDA COMPLETOS/src/main/java/GeneradorDeDatosGenericos/src/main/java/UT3/TDA/IConjunto.java
+++
@@ -1,9 +0,0 @@
-package ut3.TDA;
-
-public interface IConjunto<T> extends ILista<T> {
-
-    public IConjunto<T> union(IConjunto<T> otroConjunto);
-
-    public IConjunto<T> interseccion(IConjunto<T> otroConjunto);
-
-}
--- TDA COMPLETOS/src/main/java/GeneradorDeDatosGenericos/src/main/java/UT3/TDA/IPila.java
+++
@@ -1,9 +0,0 @@
-package ut3.TDA;
-
-public interface IPila<T> {
-    public void push(Nodo<T> nodo);
-
-    public Nodo<T> pop();
-
-    public Nodo<T> peek();
-}
